068599f9a75a75944170380f0617b027c782e375
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-03-24 23:10:22 +0100

    Remove failing example spec

--- spec/jls_api_rails_spec.rb
+++ spec/jls_api_rails_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe JlsApiRails do
   it "has a version number" do
     expect(JlsApiRails::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
