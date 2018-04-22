826341ea15cb0400a108816287081222a2f1b7ef
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-04-22 20:09:24 +0200

    Add JLS:API routes invocator to install generator

--- lib/generators/jls_api/install_generator.rb
+++ lib/generators/jls_api/install_generator.rb
@@ -1,6 +1,11 @@
 module JlsApi
   class InstallGenerator < Rails::Generators::Base
     def create_files_and_directories
+      gsub_file "config/routes.rb", "Rails.application.routes.draw do", <<-CONTENT
+Rails.application.routes.draw do
+  # JLS:API router invocation
+  jls_api :v1
+      CONTENT
       create_file "app/api/v1/models/.keep", ""
       create_file "app/api/v1/routes/default.yml", <<-CONTENT
 actions:
